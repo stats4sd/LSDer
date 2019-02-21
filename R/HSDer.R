f0473ac72198deedf5de97ec16dd7c30b27d5c9d
sdumble1 <s.dumble@stats4sd.org>
2019-02-21 07:01:04 +0000

    Add files via upload

--- R/HSDer.R
+++ R/HSDer.R
@@ -1,21 +1,21 @@
-#' Calculating Fishers LSD value from a mixed effects regression model fitted using \code{lme4}
+#' Calculating Tukey's HSD (Honestly Significant Difference) value from a mixed effects regression model fitted using \code{lme4}
 #' @param model An object of class \code{merMod}
-#' @param term string indicating variable to calculate LSDs for
-#' @param comps vector of length 2 indicating which groups to compare
-#' @param level confidence level for LSD
-#' @keywords LSD lmer
+#' @param term string indicating variable to calculate HSDs for
+#' @param comps (optional) vector of length 2 indicating which groups to compare
+#' @param level confidence level for HSD
+#' @keywords Tukey HSD lmer
 #' @export
 #' @examples
 
-LSDer<-function(model,term,comps=NULL,level=0.95){
+HSDer<-function(model,term,comps=NULL,level=0.95){
   if(class(model)=="merMod"|class(model)=="merModLmerTest"|class(model)=="lmerModLmerTest"){
     if(!term%in%colnames(model@frame))
     {stop(paste("No variable called",term,"found in model",deparse(substitute(model))))}
-    if(is.null(comps)){
     t1<-(table(model@frame[term]))
+    if(is.null(comps)){
     ns<-cbind(median(t1),median(t1))
     rownames(ns)<-NULL
-    message<-paste("LSD for comparison between two groups with",ns[1],"observations in each group")
+    message<-paste("HSD for comparison between two groups with",ns[1],"observations in each group")
     }
     else{
       if(!comps[1]%in%as.character(model@frame[term][,1])){
@@ -26,17 +26,21 @@ LSDer<-function(model,term,comps=NULL,level=0.95){
       }
       ns<-t(matrix((table(model@frame[term]))[comps]))
       names(ns)<-NULL
-      message<-paste("LSD for comparison between",comps[1],"and",comps[2])
+      message<-paste("Tukey HSD for comparison between",comps[1],"and",comps[2])
     }
     
     MS<-sigma(model)**2
     DF<-(anova(model))[term,"DenDF"]
     NREP1<-ns[,1]
     NREP2<-ns[,2]
-    LSD<-qt(1-((1-level)/2),DF)*sqrt(MS*(1/NREP1+1/NREP2))
+    nt<-length(t1)
+    HSD<-qtukey(level,nt,DF)*sqrt(MS*(0.5*(1/NREP1+1/NREP2)))
     
     cat(message,sep = "\n")
-    return(LSD)
+    return(HSD)
     }
   else(stop("model not of class merMod or merModLmerTest"))
 }
+
+
+
